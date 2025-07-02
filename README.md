04d53795782ba86cf2c727d8a16361434da47e8d
Lanreadebayo267 <oadebayo3@byupathway.edu>
2025-07-02 19:16:37 +0100

    changes made to README.md

--- README.md
+++ README.md
@@ -8,3 +8,6 @@ git add README.md
 git commit -m "Updated README with my name"
 
 git push
+
+
+dotnet --version
--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,12 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Prompt for first name
         Console.Write("What is your first name? ");
         string firstName = Console.ReadLine();
 
+        // Prompt for last name
         Console.Write("What is your last name? ");
         string lastName = Console.ReadLine();
 
-        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}. ")
+        // Print formatted result
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
